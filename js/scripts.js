7cbf1eaa9c6c64f77a18238f400d7f863a4278ad
Geoff Goetz, Jose Amesquita and Sommer Howe <jose@>
2020-01-29 11:43:07 -0800

    input player names, output player names to screen.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    $("#game").
+    $("#game").submit(function(event){
+    event.preventDefault();
+        var player1 = $("input#player-one").val();
+        var player2 = $("input#player-two").val();
+        console.log(player1 + ", " + player2)
+    
+        $("#output1").text(player1).show();
+        $("#output2").text(player2).show();
+        
+  
+    });
 });
