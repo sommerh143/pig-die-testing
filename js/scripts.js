93d9bab1f4d5f1f8c5916547d9b492c84d140e3d
Geoff Goetz, Jose Amesquita and Sommer Howe <jose@>
2020-01-30 16:14:23 -0800

    Added JS functionality. Still not functional. Unsure why.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,59 @@
-function Player (score,roll) {
+function Player (score,temp_score, tempArray) {
   this.score = score,
-  this.roll = roll
+  this.temp_score = temp_score
+  this.tempArray = tempArray
 }
 
-// Player.prototype.score = function(){
-//   rtest
-// }
+function CurrentPlayer (user){
+  this.user1 = 1,
+  this.user2 = 0
+}
 
-Player.prototype.Roll = function(){
-  // $("#roll").click(function(){
-  var roll = (Math.floor(Math.random()*6) +1);
-  if (roll === 1) {
-    alert("player2 turn");
-    roll = 0;
-    return roll;
-    
-  } else 
-    return roll;
+CurrentPlayer.prototype.Switch = function(){
   
+  if (currentplayer.user1 === 1){
+    currentplayer.user1 = 0;
+  } else {
+    currentplayer.user1 = 1 ;
+  }
+}
+
+
+Player.prototype.Turn = function(){
+
+  var x = (Math.floor(Math.random()*6) +1);
+  if (x !== 1) {
+    console.log("this is x: ", x)
+    this.temp_score += x;
+    total += this.temp_score;
+    console.log(total)
+    // sum.push(this.temp_score);
+  } else {
+    this.temp_score = 0;
+    currentplayer.Switch();
+    // currentplayer.user1 = 0;
+
+  }
+  // return this.temp_score
+  return total
 }
 
 Player.prototype.Hold = function() {
-  for (var i = 0; i < this.roll.length; i++) {
-    this.score += this.roll[i];
-    //this.score = this.score + roll[i]
-  }
-  return this.score;
+      this.tempArray.push(this.temp_score)
+      console.log(this.tempArray);
+      currentplayer.Switch();
+
+      return this.score;
 }
 
+var sum = [];
+var sum2 = [];
+var total = 0;
+var total2 = 0;
+var playerOne = new Player(0, 0, []);
+var playerTwo = new Player(0, 0, []);
+var currentplayer = new CurrentPlayer(1);
 
-
-var playerOne = new Player(0, []);
-var playerTwo = new Player(0, []);
 
 $(document).ready(function(){
   $("#game").submit(function(event){
@@ -39,51 +61,53 @@ $(document).ready(function(){
       var player1 = $("input#player-one").val();
       var player2 = $("input#player-two").val();
       console.log(player1 + ", " + player2)
-      // $("#output1").text(player1).show();
-      // $("#output2").text(player2).show();
+      $("#output1").text(player1).show();
+      $("#output2").text(player2).show();
   });
 
   $("#roll").click(function() {
-    
-    var roll1 = playerOne.Roll();
-    
-   
-    playerOne.roll.push(roll1);
-    
-    $("#scoreOut").append(roll1 + ", ");
-
-
-
-    // playerOne.rolls();
-    // if (playerOne.rolls() === 1){
-    //   console.log("stop");
-    // }
+    if  (currentplayer.user1 === 1){
+      var roll1 = playerOne.Turn();
+      $("#scoreOut").text(""+ roll1);
+    } else {
+      var roll2 = playerTwo.Turn();
+      $("#scoreOut2").text(""+ roll2);  
+    }
   });
+
+
   $("#hold").click(function() {
-    // console.log(playerOne);
-    var hold1 = playerOne.Hold();
-    // console.log(die);
-    // var re = [];
-    // playerOne.roll.push(hold1);
-    // console.log(roll);
-    $("#scoreOut2").text(hold1);
-
-
-
-    // playerOne.rolls();
-    // if (playerOne.rolls() === 1){
-    //   console.log("stop");
-    // }
+    if  (currentplayer.user1 === 1){
+      var roll1 = playerOne.Hold();
+      // for(var i = 0; i < sum.length; i++){
+        // total += sum[i];
+        // console.log(sum)
+        if (total >= 100){
+          alert("You Win")
+        }
+      }
+      console.log(playerOne.tempArray)
+    $("#scoreTotal1").text(""+ playerOne.tempArray[playerOne.tempArray.length-1]);
+    } else {
+      playerTwo.Hold();
+      // for(var i = 0; i < sum2.length; i++){
+        // score2 += sum2[i];
+        // console.log(score2)
+        // if (total >= 100){
+        //   alert("You Win")
+        // }
+      // }
+      console.log(playerOne.tempArray)
+      // $("#scoreTotal2").text(""+ playerTwo.tempArray[playerTwo.tempArray.length-1]); 
+      $("#scoreTotal2").text(total) 
+    }
   });
-  $("#roll").click(function() {
-    
-    var roll1 = playerTwo.Roll();
-    
-   
-    playerTwo.roll.push(roll1);
-    
-    $("#scoreOut3").append(roll1 + ", ");
 
-  });
-
+  // $("#roll").click(function() {
+  //   // if  (currentplayer.user1 === 1){
+  //     var roll1 = playerOne.Turn();
+  //     $("#scoreOut").text(""+ roll1);
+  //     $("#roll").hide();
+  //     $("#hold").hide();
+  // });
 });
